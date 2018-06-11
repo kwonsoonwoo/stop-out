fb50cd2d1c6be23c9550e4201166e37a0d77dbbc
kwonsoonwoo <zenuine1@gmail.com>
2018-06-11 13:57:21 +0900

    stop-out second

--- education/blog/views.py
+++ education/blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+from blog.models import School
+
+
+def school_list(request, school_id):
+    school_list = School.objects.get(id=school_id)
+    context = {
+        'school_list': school_list
+    }
+    return render(request, 'school', context)
